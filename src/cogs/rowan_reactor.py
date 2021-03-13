b1ea11a856a7e1adce7a39becb401e979f8033a1
dustpancake <fergusbkr@gmail.com>
2021-03-13 22:37:40 +0100

    rowan reactor

---
+++ src/cogs/rowan_reactor.py
@@ -0,0 +1,27 @@
+import logging
+
+import discord
+from discord.ext import commands
+
+USERNAME = "e"
+DISCRIMINATOR = "6127"
+EMOJI = "<:thee:817130256808673283>"
+
+class RowanReactor(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.logging = logging.getLogger(__name__)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.name == USERNAME and message.author.discriminator == DISCRIMINATOR:
+            self.logging.info(message.author)
+            await message.add_reaction(EMOJI)
+        
+def setup(bot):
+    bot.add_cog(
+        RowanReactor(bot)
+    )
+
+    return

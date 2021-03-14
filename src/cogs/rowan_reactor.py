f7c4213393af56d74f1cba951fa0a7f56e4b233a
Fergus Baker <fergusbkr@gmail.com>
2021-03-14 16:57:52 +0100

    Bugfixes (#8)
    * updated requirements
    
    * added pope cog
    
    * benio
    
    * pope now chooses random image
    
    * changed popelist formatting
    
    * pope gifs
    
    * removed pop gifs as were just previews
    
    * pope fixes
    
    * config and black formatting

--- src/cogs/rowan_reactor.py
+++ src/cogs/rowan_reactor.py
@@ -6,8 +6,8 @@ from discord.ext import commands
 _ID = 691729794462908487
 EMOJI = "<:thee:817130256808673283>"
 
-class RowanReactor(commands.Cog):
 
+class RowanReactor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.logging = logging.getLogger(__name__)
@@ -17,10 +17,9 @@ class RowanReactor(commands.Cog):
 
         if message.author.id == _ID:
             await message.add_reaction(EMOJI)
-        
+
+
 def setup(bot):
-    bot.add_cog(
-        RowanReactor(bot)
-    )
+    bot.add_cog(RowanReactor(bot))
 
     return

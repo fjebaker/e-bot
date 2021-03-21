78782871a0da02c83c89377bb80114df44aefe92
dustpancake <fergusbkr@gmail.com>
2021-03-21 20:07:39 +0100

    new emoji lookups

--- src/utils/lookups.py
+++ src/utils/lookups.py
@@ -1,11 +1,15 @@
-CHECKMARK = "\U00002611"
-
-NUM_EMOJI = {
+# internal key to emoji
+EMOJI_FORWARD = {
     1: "\U00000031\U000020E3",
     2: "\U00000032\U000020E3",
     3: "\U00000033\U000020E3",
     4: "\U00000034\U000020E3",
     5: "\U00000035\U000020E3",
+    "checkmark": "\U00002611",
+    "up-arrow": "\U00002B06",
+    "down-arrow": "\U00002B07",
+    "temperature": "\U0001F912",
 }
 
-EMOJI_NUM = {v: k for k, v in NUM_EMOJI.items()}
+# reverse
+EMOJI_BACKWARD = {v: k for k, v in EMOJI_FORWARD.items()}

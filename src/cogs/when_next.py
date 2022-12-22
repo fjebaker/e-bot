d7b44671926fd568925725ce8d1756d585ed2ea9
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2022-12-22 09:44:56 +0000

    Removes non-working argument parsing logic from when_next.py

--- src/cogs/when_next.py
+++ src/cogs/when_next.py
@@ -29,17 +29,8 @@ class WhenNext(commands.Cog):
     @commands.command(name="whennext")
     async def entry(self, context, cmd: str):
         self.logging.info("whennext called")
-        arg_dict = self._parse_command(cmd)
         from_date = datetime.date.today()
-        if FROM_KEY in arg_dict:
-            from_date = self._parse_date(context, arg_dict[FROM_KEY])
-            if from_date is False:
-                return
         until_date = from_date + datetime.timedelta(weeks=2)
-        if UNTIL_KEY in arg_dict:
-            until_date = self._parse_date(context, arg_dict[UNTIL_KEY])
-            if until_date is False:
-                return
         weekend_days = []
         while from_date < until_date:
             if from_date.weekday() > 4:
@@ -55,19 +46,6 @@ class WhenNext(commands.Cog):
             ),
             timeout=1,
         )
-
-    async def _parse_date(self, context, date: str):
-        try:
-            from_date = datetime.datetime.strptime(date, DATE_FORMAT)
-            return from_date.date()
-        except ValueError:
-            await context.send("Invalid start date - try the format dd/mm/yy")
-            return False
-
-    def _parse_command(self, cmd: str):
-        cmd_parts = cmd.split(" ")
-        return dict(zip(cmd_parts[::2], cmd_parts[1::2]))
-
 
 async def setup(bot):
     await bot.add_cog(WhenNext(bot))

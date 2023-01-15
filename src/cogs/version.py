ed4add7f8eeacbb0ad8820983ca087c4ffc783ce
JR Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2023-01-15 14:53:23 +0000

    Feature/49 version command (#50)
    * Adds basic version cog
    
    * Adds variables and method for semantic versioning in version.py
    
    * Fixes linting issues
    
    * Fixes reversed logic
    
    * Adds logging and more descriptive message to Version command
    
    * Reduces RC version to 0 for now
    
    * Moves the version numbers to their own top-level declaration file
    
    * Fixes lint issues in version.py
    
    * Changed the value of eversion.RELEASE_CANDIDATE from 0 to None

---
+++ src/cogs/version.py
@@ -0,0 +1,29 @@
+import logging
+
+from discord.ext import commands
+
+import eversion
+
+
+class Version(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.logging = logging.getLogger(__name__)
+
+    @commands.command(name="version")
+    async def entry(self, context):
+        current_version = self._get_version_string()
+        self.logging.info(f"version called - current version is {current_version}")
+        await context.send(f"Currently running e-bot version {current_version}")
+
+    def _get_version_string(self):
+        release_extension = ""
+        if eversion.RELEASE_CANDIDATE:
+            release_extension = f"-rc{eversion.RELEASE_CANDIDATE}"
+        return f"Currently running e-bot version {eversion.MAJOR_VERSION}.{eversion.MINOR_VERSION}.{eversion.PATCH_VERSION}{release_extension}"
+
+
+async def setup(bot):
+    await bot.add_cog(Version(bot))
+
+    return
---
+++ src/eversion.py
@@ -0,0 +1,7 @@
+# Single source of truth file for the semantic version of the project
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+PATCH_VERSION = 0
+# Note - RC versions currently not in use - but may be in the future.
+RELEASE_CANDIDATE = None

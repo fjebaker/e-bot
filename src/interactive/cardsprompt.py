f775713bc867e1d19258c2e80324e298128120de
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2024-06-01 13:32:22 +0100

    feat(games): adds the CardsSelectWinningPromptView

--- src/interactive/cardsprompt.py
+++ src/interactive/cardsprompt.py
@@ -11,10 +11,11 @@ logger = logging.getLogger(__name__)
 
 
 class CardsPrompt(discord.ui.View):
-    def __init__(self, hand: List[str], **kwargs):
+    def __init__(self, resolve_text: str, hand: List[str], **kwargs):
         super().__init__(**kwargs)
         self.result: int = None
         self.display_response: str = None
+        self.resolve_text = resolve_text
 
         for index, card in enumerate(hand):
             button = discord.ui.Button(
@@ -37,7 +38,7 @@ class CardsPrompt(discord.ui.View):
         await self.resolve(interaction)
 
     async def resolve(self, interaction: discord.Interaction):
-        text = f"Response submitted: {self.display_response}"
+        text = f"{self.resolve_text}: {self.display_response}"
 
         await interaction.response.send_message(
             content=text, delete_after=10, ephemeral=True
@@ -63,7 +64,7 @@ class CardsGetPromptView(UserUniqueView[List[str], int]):
 
         # tailor user specific modal with a timeout equal to time remaining
         hand = user_data
-        prompt = CardsPrompt(hand, timeout=self.time)
+        prompt = CardsPrompt("Result selected", hand, timeout=self.time)
         message_content = "Select a card!\n" + '\n'.join(f"{EMOJI_FORWARD[index + 1]}: {card}" for index, card in enumerate(hand))
         await interaction.response.send_message(
             content=message_content, view=prompt, ephemeral=True, delete_after=self.time
@@ -79,3 +80,36 @@ class CardsGetPromptView(UserUniqueView[List[str], int]):
 
     def required_responses(self) -> int:
         return len(self.content) - 1  # Exclude leader
+
+
+class CardsSelectWinningPromptView(UserUniqueView[List[str], int]):
+    def __init__(self, embed, leader: int, content: Dict[int, List[str]], **kwargs):
+        super().__init__(embed, "Select winner", content, **kwargs)
+        self.leader = leader
+
+    async def get_user_response(self, interaction: discord.Interaction, user_data: List[str]):
+        uid = interaction.user.id
+        if uid != self.leader:
+            await interaction.response.send_message(
+                "Only the leader can vote for the winning answer",
+                ephemeral=True,
+                delete_after=self.time,
+            )
+            return None
+
+        # tailor user specific modal with a timeout equal to time remaining
+        prompt = CardsPrompt("Winner selected", user_data, timeout=self.time)
+        await interaction.response.send_message(
+            content="Select a winner", view=prompt, ephemeral=True, delete_after=self.time
+        )
+        await prompt.wait()
+
+        logger.info(
+            "Leader %s selected winner: '%s'",
+            interaction.user.name,
+            prompt.result,
+        )
+        return prompt.result
+
+    def required_responses(self) -> int:
+        return 1
